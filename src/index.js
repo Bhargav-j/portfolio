687a6d3899ab9441a835339c4d21d7baf687c2ab
Bhargav-j <jeereddybhargav@gmail.com>
2024-02-27 03:06:42 +0530

    basename change in index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router basename="/portfolio">
+      <Routes>
+        <Route exact path="/" element={<App />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
